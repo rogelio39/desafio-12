2bbf05b79b3d22db6eb52166a3d6185679343155
Rogelio Suleta <andresrogesu@gmail.com>
2024-01-19 20:12:36 -0300

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,24 @@ import Register from './components/register/Register';
 import Login from './components/login/Login';
 import Products from './components/products/Products';
 import NewProducts from './components/newProducts/NewProducts';
-
 import './App.css'
 import Checkout from './components/checkout/Checkout';
 
 const App = () => {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/*" element={<h2>Seccion en construccion</h2>}/>
-          <Route path='/register' element={<Register/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/products' element={<Products/>} />
-          <Route path='/new-products' element={<NewProducts/>} />
-          <Route path='/checkout/:cartId' element={<Checkout/>} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/*' element={<Register />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/new-products' element={<NewProducts />} />
+          <Route path='/checkout/:cartId' element={<Checkout />} />
+          <Route path="/*" element={<h2>Seccion en construccion</h2>} />
         </Routes>
       </BrowserRouter>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
