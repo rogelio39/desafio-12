254f0ed83c266f7674816eab53793b78c9da14ff
Rogelio Suleta <andresrogesu@gmail.com>
2024-02-27 16:31:20 -0300

    se modificaron estilos en el componente registro

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -4,7 +4,7 @@ import { useAuth } from '../../context/AuthContext';
 import styles from './Register.module.css';
 
 const Register = () => {
-    const {register} = useAuth();
+    const { register } = useAuth();
 
     const formRef = useRef(null);
     const navigate = useNavigate();
@@ -14,7 +14,7 @@ const Register = () => {
             const formData = new FormData(formRef.current);
             const data = Object.fromEntries(formData);
             const respuesta = await register(data);
-            if(respuesta === 'User created'){
+            if (respuesta === 'User created') {
                 navigate('/login')
             }
         } catch (error) {
@@ -28,30 +28,37 @@ const Register = () => {
 
     return (
         <div>
-            <h1 className="login">REGISTER</h1>
-            <form id="idFormRegister" className={styles.form} onSubmit={handleSubmit} ref={formRef}>
+            <h1 className={styles.login}>REGISTER</h1>
+            <div className={styles.register}>
+                <div className={styles.registerWelcomeMessage}>
+                    <h1>WELCOME TU ROGER E-COMMERCE</h1>
+                    <p>¡Bienvenido/a! Estamos encantados de tenerte aquí. Completa el formulario a continuación para unirte a nuestra comunidad y disfrutar de todos nuestros servicios. ¡Esperamos que tengas una excelente experiencia con nosotros!</p>
+                </div>
 
-                <label htmlFor="first_name">Enter your name</label>
-                <input type="text" id="first_name" className={styles.inputName} name="first_name" required />
+                    <form id="idFormRegister" className={styles.form} onSubmit={handleSubmit} ref={formRef}>
 
-                <label htmlFor="last_name">Enter your lastname</label>
-                <input type="text" id="last_name" className={styles.inputLastName} name="last_name" required />
+                        <label htmlFor="first_name">Enter your name</label>
+                        <input type="text" id="first_name" className={styles.inputName} name="first_name" required />
 
-                <label htmlFor="age">age</label>
-                <input type="number" id="age" className={styles.inputAge} name="age" required />
+                        <label htmlFor="last_name">Enter your lastname</label>
+                        <input type="text" id="last_name" className={styles.inputLastName} name="last_name" required />
+
+                        <label htmlFor="age">age</label>
+                        <input type="number" id="age" className={styles.inputAge} name="age" required />
 
 
-                <label htmlFor="email">Enter your email</label>
-                <input type="email" id="email" className={styles.inputEmail} name="email" required />
+                        <label htmlFor="email">Enter your email</label>
+                        <input type="email" id="email" className={styles.inputEmail} name="email" required />
 
-                <label htmlFor="password">Enter your password</label>
-                <input type="password" id="password" className={styles.inputPassword} name="password" required />
+                        <label htmlFor="password">Enter your password</label>
+                        <input type="password" id="password" className={styles.inputPassword} name="password" required />
 
-                <button type="submit" className={styles.buttonRegister}>REGISTER</button>
-                <button className={styles.buttonLogin} onClick={redirectLogin}>Si ya tienes cuenta, logueate</button>
-                <button type="button" className={styles.buttonGitHub}>Ingresar con github</button>
-            </form>
-        </div>
+                        <button type="submit" className={styles.buttonRegister}>REGISTER</button>
+                        <button className={styles.buttonLogin} onClick={redirectLogin}>Si ya tienes cuenta, logueate</button>
+                        <button type="button" className={styles.buttonGitHub}>Ingresar con github</button>
+                    </form>
+                </div>
+            </div>
     )
 }
 
